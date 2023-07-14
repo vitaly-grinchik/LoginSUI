e7221776396641fb1c88a227d251d4dc8d289abc
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-07-14 16:26:48 +0300

    Initial Commit

---
+++ LoginSUI/LoginSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSUIApp.swift
+//  LoginSUI
+//
+//  Created by Виталий Гринчик on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct LoginSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
